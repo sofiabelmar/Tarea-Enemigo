82f33d84483485db6966676f370ef71cff881822
Usuario <Usuario@DESKTOP-GPB5TEB>
2018-08-15 19:44:30 -0700

    Add project files.

---
+++ ConsoleApplication4/Enemigo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Enemigo
+    {
+        public string Nombre { get; set; }
+        public int PosicionX { get; set; }
+        public int PosicionY { get; set; }
+
+        private int _Puntos;
+        public int Puntos
+        {
+            get { return _Puntos; }
+            set
+            {
+                if (value <= 100)
+                {
+                    _Puntos = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Puntos excesivos");
+                }
+            }
+        }
+
+        private int _Disparos;
+        public int Disparos
+        {
+            get { return _Disparos; }
+            set
+            {
+                if (value >= 10)
+                {
+                    
+                    Console.WriteLine("El enemigo ha muerto, ASESINO. Has ganado " + Puntos + " Puntos");
+                }
+
+                else if (value >= 5)
+                {
+                    Puntos = Puntos - 20;
+                    Console.WriteLine("Daño suficiente, El enemigo ha sido noqueado. Has Ganado " + Puntos + " Puntos");
+                }
+                else
+                {
+                    _Disparos = value;
+                    Console.WriteLine("Daño Insuficiente, Has sido vencido");
+                }
+            }
+        }
+
+        private int _Explosion;
+        public int Explosion
+        {
+            get { return _Explosion; }
+            set
+            {
+                if (value >= 3)
+                {
+                    Console.WriteLine("Has vencido Al Mounstruo! Has ganado " + Puntos + " Puntos");
+                }
+
+                else
+                {
+                    _Explosion = value;
+                    Console.WriteLine("Daño Insuficiente, Suerte a la proxima.");
+                }
+            }
+        }
+
+
+
+
+    
+
+}
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eleccion;
+
+            Enemigo enemigouno = new Enemigo();
+
+            Console.WriteLine("Nombre del Jugador: ");
+            enemigouno.Nombre = Console.ReadLine();
+
+            enemigouno.PosicionX = 10;
+            enemigouno.PosicionY = 0;
+            enemigouno.Puntos = 50;
+
+            Console.WriteLine("Posicion en X: " + enemigouno.PosicionX);
+            Console.WriteLine("Posicion en Y: " + enemigouno.PosicionY);
+
+            Console.WriteLine(" Elija 1 para Disparar o 2 para elegir");
+            eleccion = int.Parse(Console.ReadLine());
+
+            if (eleccion == 1)
+            {
+                Console.WriteLine("¿Cuantos Disparos realiza? ");
+                enemigouno.Disparos = int.Parse(Console.ReadLine());
+                Console.ReadLine();
+
+
+            }
+            else if (eleccion == 2)
+            {
+                Console.WriteLine("¿Cuantas Bombas usaras?");
+                enemigouno.Explosion = int.Parse(Console.ReadLine());
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Insertaste una opción Incorrecta");
+                Console.ReadLine();
+            }
+        }
+    }
+}
